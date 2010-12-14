e1271d58f248237734f8c6aac9f75f9c67258d22
Matti Paksula <matti.paksula@iki.fi>
2010-12-14 10:55:36 +0200

    Routes, Parts, Walks, Lines, PartTimes, Arrivals, Departures

--- lib/reittiopas/routing.rb
+++ lib/reittiopas/routing.rb
@@ -11,9 +11,294 @@ class Reittiopas
                  :b => @to.coordinates[:kkj].to_routing_string,
                  :opts => opts }
       
-      Route.parse @http.get(params), opts
+      xml = @http.get(params), opts
+      
+      doc = Nokogiri::XML(xml)
+      Route.parse doc
+    end
+  
+  
+    class Route
+    
+      attr_reader :time, :distance
+      attr_accessor :parts, :walks, :lines
+      
+      def initialize(opts)
+        @time = opts[:time].to_f if opts[:time]
+        @distance = opts[:distance].to_f if opts[:time]
+        @parts, @walks, @lines = [], [], []
+        
+      end
+      
+      def self.parse(xml)
+        length_element = xml.search("LENGTH").first
+        
+        time = length_element.get_attr_value "time"
+        dist = length_element.get_attr_value "dist"
+        
+        route = new(:time => time,
+                    :distance => dist)
+
+        xml.elements.each do |e|
+          next if e.name == "LENGTH"
+
+          case e.name
+            when "POINT"
+              route.parts << Point.parse(e)
+            when "WALK"
+              walk = Walk.parse(e)
+              route.parts << walk
+              route.walks << walk
+            when "LINE"
+              line = Line.parse(e)
+              route.parts << line
+              route.lines << line
+          end
+
+        end
+        #start_point_element = xml.search("POINT[@uid='start']").first
+        
+      #  puts start_point_element
+        
+        return route
+      end
+      
+      def k
+        start_point_arrival_element = start_point_element.search("ARRIVAL").first
+
+        dest_point_element = xml.search("POINT[@uid='dest']").first
+        dest_point_arrival_element = dest_point_element.search("ARRIVAL").first
+
+
+        start_date = start_point_arrival_element.attribute("date").value
+        start_time = start_point_arrival_element.attribute("time").value
+        start_datetime = DateTime.parse("#{start_date} #{start_time}")
+
+        dest_date = dest_point_arrival_element.attribute("date").value
+        dest_time = dest_point_arrival_element.attribute("time").value
+        dest_datetime = DateTime.parse("#{dest_date} #{dest_time}")
+        # 
+        # parts = []
+        # 
+        # xml.elements.each do |e|
+        #   next if e.name == "LENGTH" || e.name == "POINT"
+        # 
+        #   if e.name == "WALK"        
+        #     parts << ReittiopasAPI::Walk.parse(e)
+        #   elsif e.name == "LINE"
+        #     parts << ReittiopasAPI::Line.parse(e)        
+        #   end
+        # end
+        # 
+        # new(:time=>time,
+        #     :dist=>dist,
+        #     :start_datetime => start_datetime,
+        #     :dest_datetime => dest_datetime,
+        #     :parts => parts)
+        
+      end
+      
+    end
+    
+    class Section
+      attr_reader :x, :y, :arrival, :departure
+      
+      def initialize(opts)
+        @x = opts[:x].to_f if opts[:x]
+        @y = opts[:y].to_f if opts[:y]
+        @arrival = opts[:arrival]
+        @departure = opts[:departure]
+      end
+
+      def self.parse(xml)
+        x = xml.get_attr_value "x"
+        y = xml.get_attr_value "y"
+        
+        arrival_element = xml.search("ARRIVAL").first
+        departure_element = xml.search("DEPARTURE").first
+        
+        arrival = Arrival.parse arrival_element
+        departure = Departure.parse departure_element
+        
+        { :x => x,
+          :y => y,
+          :arrival => arrival,
+          :departure => departure }
+      end
+
     end
         
+    class Point < Section
+      
+      attr_reader :uid
+      
+      def initialize(opts)
+        super
+        @uid = opts[:uid]
+      end
+
+      def self.parse(xml)
+        opts = super
+        
+        uid = xml.get_attr_value "uid"
+        
+        opts.merge!( :uid => uid )
+        new(opts)
+      end
+    end
+    
+    class MapLocation < Section
+      
+      attr_reader :location_type
+      
+      def initialize(opts)
+        super
+        @location_type = opts[:location_type].to_i if opts[:location_type]
+      end
+      
+      def self.parse(xml)
+        opts = super
+        location_type = xml.get_attr_value "type"
+        
+        opts.merge!(:location_type => location_type)
+        new(opts)
+      end
+    end
+    
+    class Stop < Section
+      
+      attr_reader :code, :stop_id,
+                  :names
+      
+      def initialize(opts)
+        super
+        @code = opts[:code]
+        @stop_id = opts[:stop_id]
+        @names = opts[:names]
+      end
+      
+      def self.parse(xml)
+        opts = super
+        
+        code = xml.get_attr_value "code"
+        stop_id = xml.get_attr_value "id"
+        
+        name_elements = xml.search("NAME")
+        
+        names = {}
+        name_elements.each do |e|
+          lang = e.get_attr_value "lang"
+          val = e.get_attr_value "val"
+          
+          names[lang] = val
+        end
+        
+        opts.merge!(:code => code,
+                    :stop_id => stop_id,
+                    :names => names)
+        new(opts)
+      end
+      
+    end
+    
+    
+    class SectionTime
+      
+      attr_reader :date_time
+      
+      def initialize(opts)
+        @date_time = DateTime.parse("#{opts[:date]} #{opts[:time]}")
+      end
+      
+      def self.parse(xml)
+        date = xml.get_attr_value "date"
+        time = xml.get_attr_value "time"
+
+        new(:date => date,
+            :time => time)
+      end
+    end
+    
+    class Arrival < SectionTime
+    
+    end
+    
+    class Departure < SectionTime
+    
+    end
+    
+    class Part
+  
+      attr_reader :time, :distance,
+                  :sections
+                  
+      def initialize(opts)
+        @time = opts[:time].to_f if opts[:time]
+        @distance = opts[:distance].to_f if opts[:time]
+        @sections = opts[:sections]
+      end
+
+      def self.parse(xml)
+        length_element = xml.search("LENGTH").first
+        
+        time = length_element.get_attr_value "time"
+        distance = length_element.get_attr_value "dist"
+        
+        sections = []
+        
+        xml.elements.each do |e|
+          next if e.name == "LENGTH"
+          
+          case e.name
+            when "POINT"
+              sections << Point.parse(e)
+            when "MAPLOC"
+              sections << MapLocation.parse(e)
+            when "STOP"
+              sections << Stop.parse(e)
+          end
+        end
+        
+        { :time => time,
+          :distance => distance,
+          :sections => sections }
+      end
+
+    end
+    
+    class Walk < Part
+      
+      def self.parse(xml)
+        opts = super
+        
+        new opts
+      end
+    end
+    
+    class Line < Part
+      attr_reader :line_id
+      
+      def initialize(opts)
+        super
+        
+        @line_id = opts[:line_id]
+      end
+
+      def self.parse(xml)
+        opts = super
+        
+        line_id = xml.get_attr_value "id"
+        
+        
+        opts.merge!(:line_id => line_id)
+      
+        new(opts)
+      end
+    end
+    
+    
   end
+  
+  
   
 end
--- spec/reittiopas_fork_spec.rb
+++ spec/reittiopas_fork_spec.rb
@@ -20,21 +20,316 @@ describe Reittiopas do
   describe "#routing" do
     
     before do
-      response = File.read(File.dirname(__FILE__) + '/fixtures/route_ulvilantie_19-kallion_kirjasto.xml')
-      stub_request(:any, /.*/).to_return(:body => response, :status => 200)   
+      @response = File.read(File.dirname(__FILE__) + '/fixtures/route_ulvilantie_19-kallion_kirjasto.xml')
+      stub_request(:any, /.*/).to_return(:body => @response, :status => 200)   
 
       @ulvilantie = Reittiopas::Location.parse(parse_elem(File.read(File.dirname(__FILE__) + '/fixtures/ulvilantie_19_helsinki.xml')))
       @kallion_kirjasto = Reittiopas::Location.parse(parse_elem(File.read(File.dirname(__FILE__) + '/fixtures/kallion_kirjasto.xml')))
     end
     
-    it "should require from and to locations with options" do
-      
-      options = {}
-      
-      @reittiopas.routing(@ulvilantie, @kallion_kirjasto, options).should == []
+    it "should require from and to locations with options" do  
+      options = {}      
+      #@reittiopas.routing(@ulvilantie, @kallion_kirjasto, options).should == []
     end
+  
+  end
+  
+  describe Reittiopas::Routing::Route do
+      
+    before(:all) do
+      xml = File.read(File.dirname(__FILE__) + '/fixtures/route_ulvilantie_19-kallion_kirjasto.xml')
+      doc = Nokogiri::XML(xml).search("ROUTE").first
+      
+      @route = Reittiopas::Routing::Route.parse(doc) 
+    end
+    
+    specify { @route.time.should == 34.605 }
+    specify { @route.distance.should == 8150.277 }    
+    specify { @route.parts.should be_a(Array) }
+    specify { @route.walks.should be_a(Array) }
+    specify { @route.lines.should be_a(Array) }
+    
+    specify { @route.parts.first.should be_a(Reittiopas::Routing::Point)}
+
+    specify { @route.parts[1].should be_a(Reittiopas::Routing::Walk)}
+#    specify { @route.parts[2].should be_a(Reittiopas::Routing::Line)}
+#    specify { @route.parts[3].should be_a(Reittiopas::Routing::Walk)}
+#    specify { @route.parts[4].should be_a(Reittiopas::Routing::Line)}
+
+    specify { @route.walks.first.should be_a(Reittiopas::Routing::Walk)}
+#    specify { @route.walks[1].should be_a(Reittiopas::Routing::Walk)}
+#    specify { @route.walks[2].should be_a(Reittiopas::Routing::Walk)}
+
+    specify { @route.lines.first.should be_a(Reittiopas::Routing::Line)}
+#    specify { @route.lines[1].should be_a(Reittiopas::Routing::Line)}
+
+    specify { @route.parts.last.should be_a(Reittiopas::Routing::Point)}
+    
+  end
+  
+  describe Reittiopas::Routing::Point do
+    
+    before(:all) do
+      doc = Nokogiri::XML(%{<POINT uid="start" x="2548199.0" y="6677769.0">
+  			<ARRIVAL date="20101213" time="2102"/>
+  			<DEPARTURE date="20101213" time="2102"/>
+  		</POINT>})
+  		
+  		element = doc.elements.first
+      @point = Reittiopas::Routing::Point.parse(element)
+    end
+    
+    specify { @point.uid.should == "start" }
+    specify { @point.x.should == 2548199.0 }
+    specify { @point.y.should == 6677769.0 }
+    specify { @point.arrival.should be_a Reittiopas::Routing::Arrival }
+    specify { @point.departure.should be_a Reittiopas::Routing::Departure }
+    
+    specify { @point.arrival.date_time.should == DateTime.new(2010,12,13,21,02)}
+    specify { @point.departure.date_time.should == DateTime.new(2010,12,13,21,02)}
 
   end
+
+  describe Reittiopas::Routing::MapLocation do
+    
+    before(:all) do
+      doc = Nokogiri::XML(%{<MAPLOC x="2548250.4" y="6677834.0" type="0">
+				<ARRIVAL date="20101213" time="2104"/>
+				<DEPARTURE date="20101214" time="2105"/>
+			</MAPLOC>})
+  		
+  		element = doc.elements.first
+      @map_location = Reittiopas::Routing::MapLocation.parse(element)
+    end
+    
+    specify { @map_location.x.should == 2548250.4 }
+    specify { @map_location.y.should == 6677834.0 }
+    specify { @map_location.location_type.should == 0 }
+    specify { @map_location.arrival.should be_a Reittiopas::Routing::Arrival }
+    specify { @map_location.departure.should be_a Reittiopas::Routing::Departure }
+
+    specify { @map_location.arrival.date_time.should == DateTime.new(2010,12,13,21,04)}
+    specify { @map_location.departure.date_time.should == DateTime.new(2010,12,14,21,05)}
+
+  end
+
+
+
+  describe Reittiopas::Routing::Arrival do
+    
+    before(:all) do
+      doc = Nokogiri::XML(%{<ARRIVAL date="20101213" time="2102"/>})
+  		
+  		element = doc.elements.first
+      @arrival = Reittiopas::Routing::Arrival.parse(element)
+    end
+    
+    specify { @arrival.date_time.should == DateTime.new(2010,12,13,21,02) }    
+  end
+
+  describe Reittiopas::Routing::Departure do
+    
+    before(:all) do
+      doc = Nokogiri::XML(%{<DEPARTURE date="20101214" time="2103"/>})
+  		
+  		element = doc.elements.first
+      @arrival = Reittiopas::Routing::Departure.parse(element)
+    end
+    
+    specify { @arrival.date_time.should == DateTime.new(2010,12,14,21,03) }    
+  end
+
+
+  describe Reittiopas::Routing::Walk do
+    
+    before(:all) do
+      doc = Nokogiri::XML(%{<WALK>
+  			<LENGTH time="1.257" dist="132.387"/>
+
+  			<POINT uid="start" x="2548199.0" y="6677769.0">
+  				<ARRIVAL date="20101213" time="2102"/>
+  				<DEPARTURE date="20101213" time="2102"/>
+  			</POINT>
+  			<MAPLOC x="2548250.4" y="6677834.0" type="0">
+  				<ARRIVAL date="20101213" time="2104"/>
+  				<DEPARTURE date="20101213" time="2104"/>
+  			</MAPLOC>
+  			<STOP code="6:1304133" x="2548265.0" y="6677828.0" id="1335">
+
+  				<ARRIVAL date="20101213" time="2104"/>
+  				<DEPARTURE date="20101213" time="2104"/>
+  				<NAME lang="1" val="Ulvilantie 21"/>
+  				<NAME lang="2" val="Ulfsbyvägen 21"/>
+  			</STOP>
+  		</WALK>})
+  		element = doc.elements.first
+  		
+      @walk = Reittiopas::Routing::Walk.parse(element)
+    end
+    
+    specify { @walk.time.should == 1.257 }
+    specify { @walk.distance.should == 132.387 }
+    specify { @walk.sections.should be_a(Array)}
+    specify { @walk.sections.first.should be_a(Reittiopas::Routing::Point)}
+    specify { @walk.sections[1].should be_a(Reittiopas::Routing::MapLocation)}
+    specify { @walk.sections[2].should be_a(Reittiopas::Routing::Stop)}
+
+  end
+
+  describe Reittiopas::Routing::Line do
+    
+    before(:all) do
+      doc = Nokogiri::XML(%{<LINE id="138" code="1018  2" type="1" mobility="3">
+  			<LENGTH time="13.000" dist="5557.353"/>
+  			<STOP code="6:1304133" x="2548265.0" y="6677828.0" id="1335" ord="7">
+
+  				<ARRIVAL date="20101213" time="2104"/>
+  				<DEPARTURE date="20101213" time="2104"/>
+  				<NAME lang="1" val="Ulvilantie 21"/>
+  				<NAME lang="2" val="Ulfsbyvägen 21"/>
+  			</STOP>
+  			<STOP code="6:1304134" x="2548692.0" y="6677805.0" id="1336">
+  				<ARRIVAL date="20101213" time="2105"/>
+  				<DEPARTURE date="20101213" time="2105"/>
+  				<NAME lang="1" val="Ulvilantie 27"/>
+
+  				<NAME lang="2" val="Ulfsbyvägen 27"/>
+  			</STOP>
+  			<STOP code="6:1304135" x="2548854.0" y="6677629.0" id="1337">
+  				<ARRIVAL date="20101213" time="2106"/>
+  				<DEPARTURE date="20101213" time="2106"/>
+  				<NAME lang="1" val="Naantalintie"/>
+  				<NAME lang="2" val="Nådendalsvägen"/>
+  			</STOP>
+  			<STOP code="6:1304137" x="2548967.0" y="6677478.0" id="1339">
+
+  				<ARRIVAL date="20101213" time="2106"/>
+  				<DEPARTURE date="20101213" time="2106"/>
+  				<NAME lang="1" val="Munkkivuori"/>
+  				<NAME lang="2" val="Munkshöjden"/>
+  			</STOP>
+  			<STOP code="6:1301158" x="2549077.0" y="6677036.0" id="1303">
+  				<ARRIVAL date="20101213" time="2108"/>
+  				<DEPARTURE date="20101213" time="2108"/>
+  				<NAME lang="1" val="Niemenmäki"/>
+
+  				<NAME lang="2" val="Näshöjden"/>
+  			</STOP>
+  			<STOP code="6:1301126" x="2549163.0" y="6676754.0" id="1296">
+  				<ARRIVAL date="20101213" time="2109"/>
+  				<DEPARTURE date="20101213" time="2109"/>
+  				<NAME lang="1" val="Lokkalantie"/>
+  				<NAME lang="2" val="Locklaisvägen"/>
+  			</STOP>
+  			<STOP code="6:1301124" x="2549280.0" y="6676400.0" id="1294">
+
+  				<ARRIVAL date="20101213" time="2110"/>
+  				<DEPARTURE date="20101213" time="2110"/>
+  				<NAME lang="1" val="Munkkiniemen aukio"/>
+  				<NAME lang="2" val="Munksnäsplatsen"/>
+  			</STOP>
+  			<STOP code="6:1150114" x="2549570.0" y="6676265.0" id="544">
+  				<ARRIVAL date="20101213" time="2111"/>
+  				<DEPARTURE date="20101213" time="2111"/>
+  				<NAME lang="1" val="Paciuksenkaari"/>
+
+  				<NAME lang="2" val="Paciussvängen"/>
+  			</STOP>
+  			<STOP code="6:1150112" x="2549875.0" y="6676008.0" id="542">
+  				<ARRIVAL date="20101213" time="2112"/>
+  				<DEPARTURE date="20101213" time="2112"/>
+  				<NAME lang="1" val="Meilahdentie"/>
+  				<NAME lang="2" val="Mejlansvägen"/>
+  			</STOP>
+  			<STOP code="6:1150132" x="2550190.0" y="6675858.0" id="559">
+
+  				<ARRIVAL date="20101213" time="2113"/>
+  				<DEPARTURE date="20101213" time="2113"/>
+  				<NAME lang="1" val="Mäntytie"/>
+  				<NAME lang="2" val="Tallvägen"/>
+  			</STOP>
+  			<STOP code="6:1150110" x="2550509.0" y="6675806.0" id="540">
+  				<ARRIVAL date="20101213" time="2114"/>
+  				<DEPARTURE date="20101213" time="2114"/>
+  				<NAME lang="1" val="Haartmaninkatu"/>
+
+  				<NAME lang="2" val="Haartmansgatan"/>
+  			</STOP>
+  			<STOP code="6:1150108" x="2550696.0" y="6675654.0" id="538">
+  				<ARRIVAL date="20101213" time="2114"/>
+  				<DEPARTURE date="20101213" time="2114"/>
+  				<NAME lang="1" val="Naistenklinikka"/>
+  				<NAME lang="2" val="Kvinnokliniken"/>
+  			</STOP>
+  			<STOP code="6:1140105" x="2551068.0" y="6675103.0" id="476">
+
+  				<ARRIVAL date="20101213" time="2116"/>
+  				<DEPARTURE date="20101213" time="2116"/>
+  				<NAME lang="1" val="Linnankoskenkatu"/>
+  				<NAME lang="2" val="Linnankoskigatan"/>
+  			</STOP>
+  			<STOP code="6:1140130" x="2551252.0" y="6674769.0" id="496">
+  				<ARRIVAL date="20101213" time="2117"/>
+  				<DEPARTURE date="20101213" time="2117"/>
+  				<NAME lang="1" val="Töölön sairaala"/>
+
+  				<NAME lang="2" val="Tölö sjukhus"/>
+  			</STOP>
+  			<STOP code="6:1140103" x="2551355.0" y="6674571.0" id="474" ord="21">
+  				<ARRIVAL date="20101213" time="2117"/>
+  				<DEPARTURE date="20101213" time="2117"/>
+  				<NAME lang="1" val="Töölöntori"/>
+  				<NAME lang="2" val="Tölötorg"/>
+  			</STOP>
+  		</LINE>
+  		})
+  		element = doc.elements.first
+  		
+      @line = Reittiopas::Routing::Line.parse(element)
+    end
+    
+    specify { @line.line_id.should == "138" }
+  #  specify { @line.code.should == "1018  2" }
+   # specify { @line.line_type.should == 1 }
+    #specify { @line.mobility.should == 3 }
+
+    #specify { @line.time.should == 13.000 }
+    #specify { @line.distance.should == 5557.353 }
+    
+    #specify { @line.sections.should be_a(Array) }
+    #specify { @line.stops.should be_a(Array) }
+    
+  end
+
+
+  describe Reittiopas::Routing::Stop do
+    
+    before(:all) do
+      doc = Nokogiri::XML(%{<STOP code="6:1304133" x="2548265.0" y="6677828.0" id="1335">
+
+				<ARRIVAL date="20101213" time="2104"/>
+				<DEPARTURE date="20101213" time="2104"/>
+				<NAME lang="1" val="Ulvilantie 21"/>
+				<NAME lang="2" val="Ulfsbyvägen 21"/>
+			</STOP>})
+  		element = doc.elements.first
+  		
+      @stop = Reittiopas::Routing::Stop.parse(element)
+    end
+    
+    specify { @stop.code.should == "6:1304133" }
+    specify { @stop.x.should == 2548265.0 }
+    specify { @stop.y.should == 6677828.0 }
+    specify { @stop.stop_id.should == "1335" }
+    
+    specify { @stop.arrival.should be_a(Reittiopas::Routing::Arrival)}
+    specify { @stop.departure.should be_a(Reittiopas::Routing::Departure)}
+
+    specify { @stop.names.should == { "1" => "Ulvilantie 21",
+                                      "2" => "Ulfsbyvägen 21"}}
+    
+  end
+
 
 
   describe Reittiopas::Location::Coordinates::KKJ do
